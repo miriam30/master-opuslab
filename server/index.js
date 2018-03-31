0ec876d2901304be7807b7b8042165817251a1e2
miriam30 <miriam.alvarez.p96@gmail.com>
2018-03-31 11:51:09 -0600

    created skeleton repo

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('listening on port 3000!');
+});
