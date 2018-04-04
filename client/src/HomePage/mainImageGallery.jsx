68e2367d9fbe65592e40e6892c6301a4ab4a5f6a
Beagbarreto <beagbarreto09@gmail.com>
2018-04-03 20:07:36 -0500

    mini gallery

--- client/src/HomePage/mainImageGallery.jsx
+++ client/src/HomePage/mainImageGallery.jsx
@@ -1,1 +1,51 @@
-//image gallery
+import React from 'react';
+
+<main>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/23132084_10155107724722336_9198200496830921234_n.jpg?_nc_cat=0&oh=4fe6b1c695459bf60e95f5849c8be675&oe=5B2EE96E">
+    <h3>Workshops</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/25508215_10155222275352336_7156260847595440438_n.jpg?_nc_cat=0&oh=3ae10a01deaa9f80f0020ce678b19fd8&oe=5B69FA43">
+    <h3>The Team</h3>
+    <p>Learn more about us and what we do and what we can do for you!</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/27540623_10155322512627336_7636842989626196370_n.jpg?_nc_cat=0&oh=d3613d802b213b64c38910f0b538a4bd&oe=5B6B4CD5">
+    <h3>Documentary</h3>
+    <p>Lorem Ipsum dolor sit anem.</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/1916110_196881127335_7066113_n.jpg?_nc_cat=0&oh=8085c6dd6e99123cd53513b177eea86b&oe=5B36C4B1">
+    <h3>Video Demo</h3>
+    <p>Video Gallery with REEL.</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/14572344_10154025852017336_3565114848756687380_n.jpg?_nc_cat=0&oh=f0dcf2bae0ab08fc2d6a5f51c649cc8a&oe=5B3B11BF">
+    <h3>Content Development</h3>
+    <p>This is realización de contenido (REEL), no more than 3 lines of text so user reads all and can see there's more sections.</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/1916110_196881237335_3156112_n.jpg?_nc_cat=0&oh=f6f9a318372bd625a2387496d0b32363&oe=5B2A3A6F">
+    <h3>Photo Portfolio</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t31.0-8/737188_10151740603402336_1971631397_o.jpg?_nc_cat=0&oh=4d93335d0e4462416651b12dac43332f&oe=5B72F3B5">
+    <h3>Our Clients</h3>
+    <p>Here we go to client section. Music music here and there, music music everywhere!</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/14606429_10154051602702336_8323383656257733453_n.jpg?_nc_cat=0&oh=c42ffdee2eb43f6d5b6c25fe47d15b11&oe=5B27E14D">
+    <h3>Follow up</h3>
+    <p>Seguimiento de obra, has only external link</p>
+  </article>
+  <article>
+    <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/18700255_10154666992052336_1725961700385601626_n.jpg?_nc_cat=0&oh=8f4adca537078fb04439207cdfdd2ed8&oe=5B2AD4CB">
+    <h3>Collaboration</h3>
+    <p>Let's work together.</p>
+  </article>
+</main>
+
+export default mainImageGallery;
